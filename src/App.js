0bebc1ec82fc8e4cda9ada4719f4d8e3a4892e04
Rehaan Khan <rehaancool796@gmail.com>
2020-08-12 17:16:38 +0530

    induced new router using react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+const HatsPage = () => (
+  <div>
+      <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-        <Homepage />
+      <BrowserRouter>
+      <Switch>
+      <Route exact={true} path='/' component={Homepage} />
+      <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
