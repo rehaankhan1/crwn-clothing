5ca72fa9ab46aa037ff44123acd36081cd03d758
Rehaan Khan <rehaancool796@gmail.com>
2020-12-03 09:58:18 +0530

    Changes in MongoConnect

--- mongo.connect.js
+++ mongo.connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-//mongodb+srv://level9Nine99:shwdbhgAHGF$$@cluster0.mygli.mongodb.net/order?retryWrites=true&w=majority
+//mongodb+srv://level9Nine99:shwdbhgAHGF90@cluster0.mygli.mongodb.net/order?retryWrites=true&w=majority
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true
